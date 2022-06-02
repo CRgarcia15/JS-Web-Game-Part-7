f458a94a9fa7cfc886e5c0712ae16204effc62e4
CRgarcia15 <cg58708@gmial.com>
2022-06-02 14:01:14 -0400

    updated

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -26,32 +26,32 @@ function newNonPlayableCharacter(x, y) {
    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
-      await sleep(time)
+        await sleep(time)
         stop()
     }
 
-    function walkNorth(time) {
+    async  function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
         await sleep(time)
-         stop()
+        stop()
     }
 
-    function walkWest(time) {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
         await sleep(time)
          stop()
     }
 
-    function walkSouth(time) {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
         await sleep(time)
         stop()
     }
 
-    function stop(time) {
+    async function stop(time) {
         direction = null
         element.src = `./assets/red-character/static.gif`
         await sleep(time)
